916b5ce88c8767e1975a58aad81aa3652cbe5d23
ItsMariamTime <mjsteinblatt@gmail.com>
2016-08-05 14:51:35 -0400

    rainbow pen, eraser, crap

--- sketch.js
+++ sketch.js
@@ -1,36 +1,57 @@
+  var Red = 255;
+  var Green = 70;
+  var Blue = 60;
+
 function circle(x, y, radius){
   ellipse(x, y, radius, radius);
 }
 var value = 0;
 function setup() {
   createCanvas(640, 640);
+  
   background (0);
+  var Red = 255;
+  var Green = 160;
+  var Blue = 50;
 }
 
 function draw() {
- 
+
   noStroke();
-
- fill (255, (value -155), (value +155));
- circle (50, 50, 60);
- fill (255, 255, 255);
- circle (50, 50, 30);
- fill (255, (value+160), (value-160));
- circle (50, 50, 10);
  
-
-
- 
-}
-function mousePressed() {
-  if (value == 0) {
-    value = 255;
-  } else {
-    value = 0;
-  }
 }
 function mouseMoved(){
  
-  fill(255, 160, 160, 50);
+  fill(Red, Green, Blue);
  circle (mouseX, mouseY, 10);
+ Blue = Blue + 1;
+ Green = Green - 1;
+ Red = Red - 1;
+ if (Blue == 255){
+   Blue = 0;
+ }
+ if (Green === 0){
+   Green = 255;
+ }
+ if (Red === 0){
+   Red = 255;
+ }
+ 
+ 
 }
+
+function keyTyped() {
+  if (key === 'c') {
+    background (0);
+  } 
+
+  // uncomment to prevent any default behavior
+  // return false;
+}
+
+function mouseDragged() {
+
+    fill (0);
+   circle (mouseX, mouseY, 15);
+}
+
